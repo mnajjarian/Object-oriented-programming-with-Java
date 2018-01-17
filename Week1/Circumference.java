e54d6465f841fbcc35dbae10ad4ab8c62d282f90
Mahdi Najjarian <mnajarian@gmail.com>
2018-01-17 09:13:24 +0200

    Create Circumference.java

---
+++ Week1/Circumference.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Circumference {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type the radius: ");
+        int radius = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Circumference of the circle: " + (2 * radius) * Math.PI);
+    }
+}
