c9ab04117d98e5b44a409dce9dc5c9eba911b8b1
Mehdi <Mehdi@Mehdis-MacBook-Pro.local>
2018-01-19 00:05:31 +0200

    remove duplicate

--- Week1/Greeter.java
+++
@@ -1,9 +0,0 @@
-public class Greeter {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.print("(And all the people of the world)");
-
-    }
-
-}
